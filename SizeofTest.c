869053fe670525eeee0fd696b52d8b88a206505f
Ricann <ricann@yeah.net>
2014-10-28 11:15:27 +0800

    增加两个文件：1.计算malloc可分配内存大小 2.用sizeof计算各种类型所占空间大小

---
+++ SizeofTest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("sizeof(int) = %d\n", (int)sizeof(int));
+	printf("sizeof(unsigned int) = %d\n", (int)sizeof(unsigned int));
+	printf("sizeof(long) = %d\n", (int)sizeof(unsigned long));
+	printf("sizeof(unsigned long) = %d\n", (int)sizeof(unsigned long));
+	printf("sizeof(long long) = %d\n", (int)sizeof(long long));
+	
+	return 0;
+}	
