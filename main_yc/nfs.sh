90e37651cedd87cf6628ac3c083c0466dd02d167
RicannMac <ricann@yeah.net>
2015-04-22 11:11:10 +0800

    添加通过传入节点号自动化生成脚本

---
+++ main_yc/nfs.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mount -t nfs 192.168.7.10:/home/root/Project /mnt/nfs -o nolock,proto=tcp,nfsvers=3
---
+++ main_yc/start_service.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+./arm_control >> /dev/null &
+./armRoute >> /dev/null &
+./camctrl >> /dev/null &
+
