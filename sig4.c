61157314380cee857151e57d4c131a727a1a128e
RicannMac <ricann@yeah.net>
2015-06-08 21:12:50 +0800

    测试在信号处理函数中进行跨函数跳转
    使用setjmp/longjmp系列函数

--- sig4.c
+++ sig4.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <setjmp.h>
 
 void sigfunc(int signo);
 
+//sigjmp_buf jmpbuf;
+jmp_buf jmpbuf;
+
 int main()
 {
+  int ret;
 
   signal(SIGUSR1, sigfunc);
+
+  //ret = sigsetjmp(jmpbuf, 0);
+  ret = setjmp(jmpbuf);
+  printf("ret = %d\n", ret);
+
   printf("before pause!\n");
   pause();
   printf("after pause!\n");
@@ -16,4 +27,7 @@ int main()
 void sigfunc(int signo)
 {
   printf("Capture signal %d!\n", signo);
+  //siglongjmp(jmpbuf, 1);
+  longjmp(jmpbuf, 1);
+  exit(1);
 }
