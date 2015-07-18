abf60a4fde66277abc03f98b22921c146408334c
RicannUbuntu <ricann@yeah.net>
2015-07-17 23:29:41 -0700

    update latest Makefile in frame module

---
+++ src/frame/make.h
@@ -0,0 +1,21 @@
+
+FRAME_INC_DIR		= $(FRAME_BASE)
+FRAME_SRC_DIR		= $(FRAME_BASE)
+FRAME_OBJ_DIR		= $(FRAME_BASE)
+
+FRAME_DIRS		= $(FRAME_INC_DIR)  $(FRAME_SRC_DIR)  $(FRAME_OBJ_DIR)
+FRAME_SRC		= $(wildcard $(FRAME_SRC_DIR)/*.c)
+
+FRAME_OBJ		= $(FRAME_SRC:.c=.o)
+FRAME_OBJ		:= $(filter %.o, $(FRAME_OBJ))
+FRAME_OBJ		:= $(patsubst $(FRAME_SRC_DIR)/%, $(FRAME_OBJ_DIR)/%, $(FRAME_OBJ)) 
+
+FRAME_DEP		= $(FRAME_SRC:.c=.dep)
+FRAME_DEP		:= $(filter %.dep, $(FRAME_DEP))
+FRAME_DEP		:= $(patsubst $(FRAME_SRC_DIR)/%, $(FRAME_OBJ_DIR)/%, $(FRAME_DEP)) 
+
+FRAME_INC		= 
+
+FRAME_CFLAGS	= $(CFLAGS)
+
+
